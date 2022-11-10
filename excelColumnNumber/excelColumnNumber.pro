140062bde5e09a0de6c450870c124ec2bb9ce2f6
Jay Two <j2doll@gmail.com>
2022-11-10 10:23:29 +0900

    ver 0.1

--- excelColumnNumber/excelColumnNumber.pro
+++ excelColumnNumber/excelColumnNumber.pro
@@ -16,12 +16,13 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-# INCLUDE += .
+INCLUDEPATH += .
 
-# HEADERS += \
-# .h
+HEADERS += \
+excelColumnNumber.h
 
 SOURCES += \
+excelColumnNumber.cpp \
 main.cpp
 
 # Default rules for deployment.
--- excelColumnNumber/main.cpp
+++ excelColumnNumber/main.cpp
@@ -15,20 +15,23 @@
 
 int main(int argc, char *argv[])
 {
-    std::cout << excelColumnNumber::convertFromNumberToExcelColumn( 0 ) << std::endl ;
-    std::cout << excelColumnNumber::convertFromNumberToExcelColumn( 1 ) << std::endl ;
-    std::cout << excelColumnNumber::convertFromNumberToExcelColumn( 100 ) << std::endl ;
-    std::cout << excelColumnNumber::convertFromNumberToExcelColumn( UINT_MAX ) << std::endl ;
+    std::cout << " [1] ------------ " << std::endl ;
+
+    std::cout << "   0 : " << excelColumnNumber::convertFromNumberToExcelColumn( 0 ) << std::endl ;
+    std::cout << "   1 : " << excelColumnNumber::convertFromNumberToExcelColumn( 1 ) << std::endl ;
+    std::cout << "   2 : " << excelColumnNumber::convertFromNumberToExcelColumn( 2 ) << std::endl ;
+    std::cout << " 100 : " << excelColumnNumber::convertFromNumberToExcelColumn( 100 ) << std::endl ;
+    std::cout << " UINT_MAX : " << excelColumnNumber::convertFromNumberToExcelColumn( UINT_MAX ) << std::endl ;
 	
-	std::cout << " ------------ " << std::endl ; 
+    std::cout << " [2] ------------ " << std::endl ;
 
-    std::cout << excelColumnNumber::alphabetToNumber('a') << std::endl ;
-    std::cout << excelColumnNumber::alphabetToNumber('z') << std::endl ;
-    std::cout << excelColumnNumber::alphabetToNumber('A') << std::endl ;
-    std::cout << excelColumnNumber::alphabetToNumber('Z') << std::endl ;
-    std::cout << excelColumnNumber::alphabetToNumber('5') << std::endl ;
+    std::cout << " a : " << excelColumnNumber::alphabetToNumber('a') << std::endl ;
+    std::cout << " z : " << excelColumnNumber::alphabetToNumber('z') << std::endl ;
+    std::cout << " A : " << excelColumnNumber::alphabetToNumber('A') << std::endl ;
+    std::cout << " Z : " << excelColumnNumber::alphabetToNumber('Z') << std::endl ;
+    std::cout << " 5 : " << excelColumnNumber::alphabetToNumber('5') << std::endl ;
 
-    std::cout << " ------------ " << std::endl ;
+    std::cout << " [3] ------------ " << std::endl ;
 
     unsigned int debug1 = excelColumnNumber::excelColumnStingToNumber("AB");
     std::string debug2 = excelColumnNumber::convertFromNumberToExcelColumn( debug1 );
